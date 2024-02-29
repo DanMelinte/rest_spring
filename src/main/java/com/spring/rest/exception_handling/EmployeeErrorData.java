bc2b32674305be536705ed5dd4c514bda7872941
DanMelinte <Dmelinte2595@gmail.com>
2024-02-29 19:05:19 +0200

    Initial commit

---
+++ src/main/java/com/spring/rest/exception_handling/EmployeeErrorData.java
@@ -0,0 +1,17 @@
+package com.spring.rest.exception_handling;
+
+public class EmployeeErrorData {
+    private String info;
+
+    public EmployeeErrorData() {
+
+    }
+
+    public String getInfo() {
+        return info;
+    }
+
+    public void setInfo(String info) {
+        this.info = info;
+    }
+}
